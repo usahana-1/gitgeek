0bfc8671597bf4eb0c719cda5a14b06c463d22e3
usahana-1 <mo4mo4601@gmail.com>
2021-11-28 01:18:37 +0900

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'hello/index' => 'hello#index'
+  root 'hello#index'
+  get 'hello/link' => 'hello#link'
+  get 'tweets' => 'tweets#index'
+  get 'tweets/new' => 'tweets#new'
+  post 'tweets' => 'tweets#create'
+  get 'tweets/:id' => 'tweets#show',as: 'tweet'
+  patch 'tweets/:id' => 'tweets#update'
+  delete 'tweets/:id' => 'tweets#destroy' 
+  get 'tweets/:id/edit' => 'tweets#edit', as:'edit_tweet'
+  
+end
